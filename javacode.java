be3aa824367be55d9d5be99313797b16b137d6a5
koteswaraonemalikanti-2502 <koteswaraonemalikanti@gmail.com>
2026-01-29 13:52:21 +0530

    this is my 2nd update

--- javacode.java
+++ javacode.java
@@ -15,7 +15,8 @@ public class SingletonTest {
     public static void main(String[] args) {
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
 
-        System.out.println(s1 == s2); // true
+        System.out.println(s1 == s2 ==s3); // true
     }
 }
